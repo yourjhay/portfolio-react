8260a170ff43717266ac7e33e738e01ad9ce7da9
Rey Jhon <reyjhonbaquirin@yahoo.com>
2021-10-22 22:35:45 +0800

    Update to support legacy browser

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['ie >= 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
+  ],
   publicDir: 'public',
   build: {
     outDir: 'build'
-  }
+  },
+
 })
