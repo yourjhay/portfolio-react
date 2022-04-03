fd03b51b2b760205f1e1383729bebf9adc6d8c9f
Rey Jhon <reyjhonbaquirin@yahoo.com>
2022-04-03 10:36:50 +0800

    Update Home layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,24 +13,24 @@ const Home = () => {
       </Command>
       <div className="mt-3">
         <p>
-          Hello! Welcome to my site. Please check available commands below to
-          show my information.
+          Hello! Welcome to my site. Interested about me? Please check available
+          commands below to show my information.
         </p>
       </div>
       <div className="mt-3">
         <p className="text-gray-500">Show Profile Information:</p>
         <p className="text-blue-400">cat ~/info/about.md</p>
         <p className="text-blue-400">about</p>
-        <p className="mt-3 text-gray-500">
+        <p className="mt-3 text-gray-500">To go to my LinkedIn profile:</p>
+        <p className="text-blue-400">linkedin</p>
+        <p className="mt-3 text-gray-500">To go to my Github profile:</p>
+        <p className="text-blue-400">github</p>
+        <p className="mt-5 text-gray-500">
           To go back here or to display available commands:
         </p>
         <p className="text-blue-400">command --help</p>
         <p className="text-blue-400">command --available</p>
         <p className="text-blue-400">cd /home</p>
-        <p className="mt-3 text-gray-500">To go to my LinkedIn profile:</p>
-        <p className="text-blue-400">linkedin</p>
-        <p className="mt-3 text-gray-500">To go to my Github profile:</p>
-        <p className="text-blue-400">github</p>
       </div>
     </TerminalContainer>
   );
