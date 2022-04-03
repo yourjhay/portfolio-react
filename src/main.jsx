0959c21b800f49a5abb9f3741045c93e8a05da07
Rey Jhon <reyjhonbaquirin@yahoo.com>
2022-04-03 10:49:19 +0800

    Update main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+import { createBrowserHistory } from 'history';
+const history = createBrowserHistory();
+let root = document.getElementById('root');
+if(root) {
+    // 1. Set up the browser history with the updated location
+    // (minus the # sign)
+    const path = (/#!(\/.*)$/.exec(location.hash) || [])[1];
+    if (path) {
+      history.replace(path);
+    }
+  
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+}
